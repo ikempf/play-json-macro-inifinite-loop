5ed160e7080043b6b5f9a04a1c31210c6da638d0
Ilja <ilja.kempf@gmail.com>
2018-09-27 13:26:34 +0200

    Infinite loop snippet

--- src/main/scala/com/ikempf/Main.scala
+++ src/main/scala/com/ikempf/Main.scala
@@ -7,8 +7,11 @@ object Main extends App {
   sealed trait Foo
   case class Bar(a: String) extends Foo
 
-  implicit val writesFoo: Writes[Foo] = Writes { case bar: Bar => Json.toJson(bar) }
   implicit val writesBar: Writes[Bar] = Json.writes[Bar]
+
+  implicit val writesFoo: Writes[Foo] = Writes {
+    case bar: Bar => Json.toJson(bar)
+  }
 
   writesFoo.writes(Bar(""))
 
